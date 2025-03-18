517f6b3f2e6ce84738ca7b3e83f3640ac90dc7d6
Andres Canales <andrescamo02@gmail.com>
2025-03-17 19:46:06 -0600

    images settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // 👈 Importante para Cloudflare Pages
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+});
